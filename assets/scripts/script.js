1a31d185bd9d5da03f890113ba1a7d0e18d61454
Lucas Ribeiro <luc4s.rib3iro@gmail.com>
2019-11-25 23:11:55 -0300

    ajustes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,73 +4,73 @@ function scrollUp() {
 }
 
 function hover1(element) {
-    element.setAttribute('src', 'assets/images/IBITINGA_CORETO.png');
+    element.setAttribute('src', 'assets/images/IBITINGA_CORETO.jpg');
 }
   
 function unhover1(element) {
-    element.setAttribute('src', 'assets/images/IBITINGA_CORETO-SELECAO.png');
+    element.setAttribute('src', 'assets/images/IBITINGA_CORETO-SELECAO.jpg');
 }
 
 function hover2(element) {
-    element.setAttribute('src', 'assets/images/BANDA-DO-CARMO.png');
+    element.setAttribute('src', 'assets/images/BANDA-DO-CARMO.jpg');
 }
   
 function unhover2(element) {
-    element.setAttribute('src', 'assets/images/BANDA-DO-CARMO-SELECAO.png');
+    element.setAttribute('src', 'assets/images/BANDA-DO-CARMO-SELECAO.jpg');
 }
 
 function hover3(element) {
-    element.setAttribute('src', 'assets/images/BANDA-DE-PIRASSUNUNGA.png');
+    element.setAttribute('src', 'assets/images/BANDA-DE-PIRASSUNUNGA.jpg');
 }
   
 function unhover3(element) {
-    element.setAttribute('src', 'assets/images/BANDA-DE-PIRASSUNUNGA-SELECAO.png');
+    element.setAttribute('src', 'assets/images/BANDA-DE-PIRASSUNUNGA-SELECAO.jpg');
 }
 
 function hover4(element) {
-    element.setAttribute('src', 'assets/images/BANCO-COSMOPOLIS.png');
+    element.setAttribute('src', 'assets/images/BANCO-COSMOPOLIS.jpg');
 }
   
 function unhover4(element) {
-    element.setAttribute('src', 'assets/images/BANCO-COSMOPOLIS-SELECAO.png');
+    element.setAttribute('src', 'assets/images/BANCO-COSMOPOLIS-SELECAO.jpg');
 }
 
 function hover5(element) {
-    element.setAttribute('src', 'assets/images/CORETO_ITU.png');
+    element.setAttribute('src', 'assets/images/CORETO_ITU.jpg');
 }
   
 function unhover5(element) {
-    element.setAttribute('src', 'assets/images/CORETO_ITU-SELECAO.png');
+    element.setAttribute('src', 'assets/images/CORETO_ITU-SELECAO.jpg');
 }
 
 function hover6(element) {
-    element.setAttribute('src', 'assets/images/PIRASSUNUNGA-CORETO.png');
+    element.setAttribute('src', 'assets/images/PIRASSUNUNGA-CORETO.jpg');
 }
   
 function unhover6(element) {
-    element.setAttribute('src', 'assets/images/PIRASSUNUNGA-CORETO-SELECAO.png');
+    element.setAttribute('src', 'assets/images/PIRASSUNUNGA-CORETO-SELECAO.jpg');
 }
 
 function hover7(element) {
-    element.setAttribute('src', 'assets/images/CORNETAS.png');
+    element.setAttribute('src', 'assets/images/CORNETAS.jpg');
 }
   
 function unhover7(element) {
-    element.setAttribute('src', 'assets/images/CORNETAS-SELECAO.png');
+    element.setAttribute('src', 'assets/images/CORNETAS-SELECAO.jpg');
 }
 
 function hover8(element) {
-    element.setAttribute('src', 'assets/images/CORETO_COSMOPOLIS.png');
+    element.setAttribute('src', 'assets/images/CORETO_COSMOPOLIS.jpg');
 }
   
 function unhover8(element) {
-    element.setAttribute('src', 'assets/images/CORETO_COSMOPOLIS-SELECAO.png');
+    element.setAttribute('src', 'assets/images/CORETO_COSMOPOLIS-SELECAO.jpg');
 }
 
 function hover9(element) {
-    element.setAttribute('src', 'assets/images/MAESTRO-ISAIAS-ITU.png');
+    element.setAttribute('src', 'assets/images/MAESTRO-ISAIAS-ITU.jpg');
 }
   
 function unhover9(element) {
-    element.setAttribute('src', 'assets/images/MAESTRO-ISAIAS-ITU-SELECAO.png');
+    element.setAttribute('src', 'assets/images/MAESTRO-ISAIAS-ITU-SELECAO.jpg');
 }
